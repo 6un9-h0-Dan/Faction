5cb23587694a4c45b681fba4245b4e6d7ea8691a
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-03-10 12:00:55 -0700

    fixes

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ sudo add-apt-repository \
 sudo apt-get update
 
 # Install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
